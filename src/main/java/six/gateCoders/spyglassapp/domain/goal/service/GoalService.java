a81b334b93ac30250f1ca44eb605259b7dc71647
Khalil Brewington <kbrewing02@gmail.com>
2022-08-03 15:57:03 -0400

    updates

--- src/main/java/six/gateCoders/spyglassapp/domain/goal/service/GoalService.java
+++ src/main/java/six/gateCoders/spyglassapp/domain/goal/service/GoalService.java
@@ -4,12 +4,20 @@ import six.gateCoders.spyglassapp.domain.core.exceptions.ResourceCreationError;
 import six.gateCoders.spyglassapp.domain.core.exceptions.ResourceNotFoundException;
 import six.gateCoders.spyglassapp.domain.goal.model.Goal;
 
+import java.util.Date;
+import java.util.Optional;
+
 public interface GoalService {
     Iterable<Goal> getall();
     Goal create (Goal goal) throws ResourceCreationError;
     Goal getById(Long id) throws ResourceNotFoundException;
-    Goal getByName(String goalName) throws ResourceNotFoundException;
-    Goal update(Long id, Goal goalDetail) throws ResourceNotFoundException;
+    Optional<Goal> getByName(String goalName) throws ResourceNotFoundException;
+    Goal updateName(Long id, String goalName) throws ResourceNotFoundException;
+    Goal updateTargetDate(Long id, Date targetDate) throws ResourceNotFoundException;
+    Goal updateDescription(Long id, String description) throws ResourceNotFoundException;
+    Goal updateCurrentDollarAmount(Long id, double currentDollarAmount) throws ResourceNotFoundException;
+    Goal updateTargetDollarAmount(Long id, double targetDollarAmount) throws ResourceNotFoundException;
+
     void delete (Long Id) throws ResourceNotFoundException;
 
 }
