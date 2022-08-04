ac44c1adbdcd080be28f7250615821f64936f7d0
degre23 <98403668+degre23@users.noreply.github.com>
2022-08-04 12:03:15 -0400

    added sql

---
+++ src/main/resources/sql/profile.sql
@@ -0,0 +1,7 @@
+create table `profile`(
+	`id` Integer primary key auto_increment,
+	`first_name` varchar(50),
+    `last_name` varchar(50),
+    `email` varchar(50),
+    `password` varchar(50)
+);
